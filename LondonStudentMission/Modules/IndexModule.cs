72ae61abd9376a5a8ba4a8637da155c5b29cdc6d
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2014-05-07 18:19:34 +0100

    separate module folder

---
+++ LondonStudentMission/Modules/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace LondonStudentMission.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters => View["index"];
+        }
+    }
+}
